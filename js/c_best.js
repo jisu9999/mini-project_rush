bb53f636ff088a439acfa9c6ada59688d1ffee26
kimjisu <ddc02336@gmail.com>
2025-02-18 17:14:27 +0900

    -best 완료 -new css작성중

---
+++ js/c_best.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    const swiper = new Swiper(".bestSwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}
