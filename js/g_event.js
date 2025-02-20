663b98c0f187045ca03a3a1c278f36588359e615
kimgihye <kimgihye55@gmail.com>
2025-02-20 17:00:23 +0900

    header-별도파일 작업중

--- js/g_event.js
+++ js/g_event.js
@@ -1,12 +1,13 @@
-// window.onload = function () {
-//   var swiper = new Swiper(".eventSwiper", {
-//     slidesPerView: 3,
-//     pagination: {
-//       el: ".g_event .swiper-pagination",
-//       clickable: true,
-//       renderBullet: function (index, className) {
-//         return '<span class="' + className + '">' + (index + 1) + "</span>";
-//       },
-//     },
-//   });
-// };
+window.addEventListener("load",function(){
+    var swiper = new Swiper(".eventSwiper", {
+         slidesPerView: 3,
+         pagination: {
+           el: ".g_event .swiper-pagination",
+           clickable: true,
+           renderBullet: function (index, className) {
+             return '<span class="' + className + '">' + (index + 1) + "</span>";
+           },
+         },
+       });
+ })
+ 
