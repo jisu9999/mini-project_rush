7eb527e390f09d1ad8dd90517fd6a759e05b00f5
kimjisu <ddc02336@gmail.com>
2025-02-20 14:41:16 +0900

    -css

--- js/c_best.js
+++ js/c_best.js
@@ -1,14 +1,26 @@
-window.onload = function(){
-    const swiper = new Swiper(".bestSwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-}
+window.onload = function () {
+  const swiper1 = new Swiper(".bestSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper1-next",
+      prevEl: ".swiper1-prev",
+    },
+  });
+  const swiper2 = new Swiper(".newSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper2-next",
+      prevEl: ".swiper2-prev",
+    },
+  });
+};
