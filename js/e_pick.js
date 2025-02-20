7e9915b1c6c43e30e6400399f0a3846dbc077444
kimjisu <ddc02336@gmail.com>
2025-02-20 17:00:26 +0900

    -pick 자바스크립트

---
+++ js/e_pick.js
@@ -0,0 +1,11 @@
+window.addEventListener("load" , function(){
+    const pickBtn = document.querySelectorAll(".pick-btn")
+    const pickBox = document.querySelectorAll("#pickBox")
+    pickBtn.forEach(function(btn){
+        btn.addEventListener("click", function(){
+            btn
+        })
+    })
+
+    })
+})
