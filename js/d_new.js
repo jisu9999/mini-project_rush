8644603b8f6e10471a2c25987b7adaf5f7b21d30
kimjisu <ddc02336@gmail.com>
2025-02-24 17:28:43 +0900

    best 반응형-ing

--- js/d_new.js
+++ js/d_new.js
@@ -1,14 +1,20 @@
-window.addEventListener("load", function(){
-    const swiper2 = new Swiper(".newSwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-          el: ".newSwiper .swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper2-next",
-          prevEl: ".swiper2-prev",
-        },
-      });
-})
+window.addEventListener("load", function () {
+  const swiper2 = new Swiper(".newSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".newSwiper .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper2-next",
+      prevEl: ".swiper2-prev",
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 4, //브라우저가 769보다 클 때
+        spaceBetween: 20,
+      },
+    },
+  });
+});
