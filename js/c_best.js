1b02e215099061c4994747a88b67151eb398daf3
kimjisu <ddc02336@gmail.com>
2025-02-24 12:54:09 +0900

    반응형ing

--- js/c_best.js
+++ js/c_best.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
   const swiper1 = new Swiper(".bestSwiper", {
-    slidesPerView: 4,
+    slidesPerView: 1.5,
     spaceBetween: 30,
     pagination: {
       el: ".bestSwiper .swiper-pagination",
@@ -9,6 +9,24 @@ window.addEventListener("load", function () {
     navigation: {
       nextEl: ".swiper1-next",
       prevEl: ".swiper1-prev",
+    },
+    breakpoints: {
+      450: {
+        slidesPerView: 2, //브라우저가 768보다 클 때
+        spaceBetween: 30,
+      },
+      690: {
+        slidesPerView: 3, //브라우저가 768보다 클 때
+        spaceBetween: 30,
+      },
+      900: {
+        slidesPerView: 4, //브라우저가 768보다 클 때
+        spaceBetween: 30,
+      },
+      // 900: {
+      //   slidesPerView: 4, //브라우저가 768보다 클 때
+      //   spaceBetween: 30,
+      // },
     },
   });
 });
--- js/e_pick.js
+++ js/e_pick.js
@@ -1,11 +1,20 @@
-window.addEventListener("load" , function(){
-    const pickBtn = document.querySelectorAll(".pick-btn")
-    const pickBox = document.querySelectorAll("#pickBox")
-    pickBtn.forEach(function(btn){
-        btn.addEventListener("click", function(){
-            btn
-        })
-    })
+// 처음 페이지 로드 시 첫번째 섹션을 보이게 설정
+window.onload = function () {
+  openTab("fresh", document.querySelector(".pickBtn"));
+};
+function openTab(sectionId, btn) {
+  // 모든 섹션 숨기기
+  const contents = document.querySelectorAll(".pick-box");
+  contents.forEach((content) => (content.style.display = "none"));
+  // 모든 버튼에서 'active' 클래스 제거
+  const buttons = document.querySelectorAll(".pickBtn");
+  buttons.forEach((button) => button.classList.remove("active"));
 
-    })
-})
+  // 선택한 섹션만 보이게 하기
+  const selectedSection = document.getElementById(sectionId);
+  // console.log(selectedSection);
+  selectedSection.style.display = "flex";
+  
+  // 클릭된 버튼에 'active' 클래스 추가하여 색상 변경
+  btn.classList.add("active");
+}
